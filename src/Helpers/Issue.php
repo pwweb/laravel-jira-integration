47a05e322ef38fd87c0855d2b98ec35246828fa2
PW*Websolutions <git@pw-websolutions.com>
2021-01-04 22:14:30 +0100

    ✳️ Adds create functionality for issues

--- src/Helpers/Issue.php
+++ src/Helpers/Issue.php
@@ -7,6 +7,16 @@ use JiraRestApi\Issue\IssueField;
 use JiraRestApi\JiraException;
 use JiraRestApi\Issue\Issue as JiraIssue;
 
+/**
+ * PWWEB\Jira\Helpers\Issue Helper.
+ *
+ * Wrapper for Jira Issues.
+ *
+ * @author    FP <fp@pw-websolutions.com>
+ * @author    RAB <rab@pw-websolutions.com>
+ * @copyright 2021 pw-websolutions.com
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
 class Issue
 {
     /**
@@ -35,7 +45,7 @@ class Issue
      *
      * @param array $parameters Parameters for retrieving issue details.
      *
-     * @return JiraRestApi\Issue\Issue|null
+     * @return \JiraRestApi\Issue\Issue|null
      */
     public function get($parameters = []): ?JiraIssue
     {
@@ -46,9 +56,8 @@ class Issue
         try {
             if (true === empty($parameters)) {
                 $parameters = [
-                    'fields' => [  // default: '*all'
-                        'summary',
-                        'comment',
+                    'fields' => [
+                        // default: '*all'
                     ],
                     'expand' => [
                         'renderedFields',
@@ -70,12 +79,47 @@ class Issue
         }
     }
 
-    public function update()
+    /**
+     * Create a new Jira issue.
+     *
+     * @param array $issueData Data to be used for creation.
+     *
+     * @return \JiraRestApi\Issue\Issue
+     */
+    public function create(array $issueData): JiraIssue
+    {
+        try {
+            $issueField = new IssueField();
+
+            foreach ($issueData as $key => $value) {
+                if (true === str_starts_with($key, 'customfield_')) {
+                    $issueField->addCustomField($key, $value);
+                } else {
+                    $issueField->{'set'.$key}($value);
+                }
+            }
+
+            $issue = $this->request->create($issueField);
+
+            return $issue;
+        } catch (JiraRestApi\JiraException $e) {
+            print("Error Occured! " . $e->getMessage());
+        }
+    }
+
+    /**
+     * Updates a Jira issue
+     *
+     * @param array $issueData Data to be updated.
+     *
+     * @return \JiraRestApi\Issue\Issue
+     */
+    public function update(array $issueData): JiraIssue
     {
         try {
             $issueField = new IssueField(true);
 
-            $issueField->addCustomField('Public Votes', '30');
+            $issueField->addCustomField('customfield_10060', 30);
 
             // optionally set some query params
             $editParams = [
@@ -83,9 +127,9 @@ class Issue
             ];
 
             // You can set the $paramArray param to disable notifications in example
-            $ret = $this->request->update($this->issueIdOrKey, $issueField, $editParams);
+            $issue = $this->request->update($this->issueIdOrKey, $issueField, $editParams);
 
-            var_dump($ret);
+            return $issue;
         } catch (JiraRestApi\JiraException $e) {
             $this->assertTrue(false, "update Failed : " . $e->getMessage());
         }
